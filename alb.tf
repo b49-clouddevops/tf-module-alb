d592c7c04c01886c202667ee11f71e0680a6818c
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 08:20:51 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "alb" {
-  name               = "test-lb-tf"
-  internal           = false
+  name               = var.ALB_NAME
+  internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [for subnet in aws_subnet.public : subnet.id]
