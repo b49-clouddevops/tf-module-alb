16730f017c5b7d8355cac8625811c76a8cfc37db
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 08:22:59 +0530

    Auto Push Commit

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "alb_public" {
+  name        = "roboshop-public-alb-${var.ENV}"
+  description = "roboshop-public-alb-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-mongodb-${var.ENV}"
+  }
+}
