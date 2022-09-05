1fa7caf0f754c43ae23743d303a81f15ba15b58a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 08:24:06 +0530

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -1,3 +1,5 @@
+
+# Public LoadBalancer SG
 resource "aws_security_group" "alb_public" {
   name        = "roboshop-public-alb-${var.ENV}"
   description = "roboshop-public-alb-${var.ENV}"
@@ -20,6 +22,34 @@ resource "aws_security_group" "alb_public" {
   }
 
   tags = {
-    Name = "roboshop-mongodb-${var.ENV}"
+    Name = "roboshop-public-alb-${var.ENV}"
+  }
+}
+
+
+# Private LoadBalancer SG
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop-private-alb-${var.ENV}"
+  description = "roboshop-private-alb-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-private-alb-${var.ENV}"
   }
 }
