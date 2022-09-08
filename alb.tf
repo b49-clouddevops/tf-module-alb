067dd4d2eecd7aed9bbfeb98643804e9215ff144
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 17:40:52 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -2,8 +2,10 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  # subnets            = If you're running the Public Module, LB should be attached under the public subnets. If it's running against private, then this has to be attached
+  #                      to private subnets.
+  subnets            =  ???
 
   enable_deletion_protection = true
 
