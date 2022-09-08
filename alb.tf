ec38792cafb1f7f70e36e6041883d6fd863cd3dc
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 17:51:47 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "alb" {
   # subnets            = If you're running the Public Module, LB should be attached under the public subnets. If it's running against private, then this has to be attached
   #                      to private subnets.
   subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID
-  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.*.id] : [aws_security_group.alb_public.*.id] 
+  security_groups    = var.INTERNAL ? aws_security_group.alb_private.*.id : aws_security_group.alb_public.*.id 
 
   enable_deletion_protection = true
 
