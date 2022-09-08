a45a9463afedf5643033547f67ceeb504af83fc7
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 17:43:11 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "alb" {
   load_balancer_type = "application"
   # subnets            = If you're running the Public Module, LB should be attached under the public subnets. If it's running against private, then this has to be attached
   #                      to private subnets.
-  subnets            = ???
+  subnets            = var.INTERNAL
   security_groups    = ????
 
   enable_deletion_protection = true
@@ -19,3 +19,7 @@ resource "aws_lb" "alb" {
     Environment = "production"
   }
 }
+
+
+# using the conditions
+# Ref: https://www.terraform.io/language/expressions/conditionals
